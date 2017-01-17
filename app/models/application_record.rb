bd52ff10b51a6384f45e8b6588cb761f16710b3d
epochyeon <harsh.parekh87@gmail.com>
2017-01-17 18:34:28 -0500

    Authentication code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+class AddAccessTokenToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :access_token, :string
+    add_column :users, :username, :string
+  end
+end
